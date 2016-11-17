c511863260633eedb305aeccd7fc41774af7ebaf
renato <renato@knewin.com>
2016-11-17 03:33:36 -0200

    Alterado controle de threads.

--- src/main/java/br/ufsc/grad/renatoback/tcc/customer/service/rest/CustomerService.java
+++ src/main/java/br/ufsc/grad/renatoback/tcc/customer/service/rest/CustomerService.java
@@ -3,15 +3,18 @@ package br.ufsc.grad.renatoback.tcc.customer.service.rest;
 import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.RestTemplate;
+
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.AsyncHttpClientConfig;
 
 @Service
 public class CustomerService {
@@ -27,8 +30,11 @@ public class CustomerService {
 
 	AtomicInteger counter = new AtomicInteger();
 
-	@Autowired
-	RemoteConfig remoteConfig;
+	RemoteConfig rc;
+
+	public CustomerService(RemoteConfig rc) {
+		this.rc = rc;
+	}
 
 	public void createCustomer() {
 		_doProcessing();
@@ -80,29 +86,83 @@ public class CustomerService {
 		}
 	}
 
+	private AsyncHttpClient asyncHttpClient;
+
+	boolean followRedirects = true;
+	boolean connectionPooling = true;
+	ExecutorService httpExecutorService;
+
+	private void configAsyncHttpClient(int threads, int interval) {
+		System.err.println("Configurando novo client...");
+		httpExecutorService = new ThreadPoolExecutor(0, threads * 2, interval, TimeUnit.SECONDS,
+				new SynchronousQueue<>());
+		// httpExecutorService = Executors.newFixedThreadPool(threads);
+		AsyncHttpClientConfig config = new AsyncHttpClientConfig.Builder().setExecutorService(httpExecutorService)
+				.setMaxConnectionsPerHost(1000).setAllowPoolingConnections(connectionPooling)
+				.setAllowPoolingSslConnections(connectionPooling).setConnectTimeout((int) TimeUnit.SECONDS.toMillis(1))
+				.setRequestTimeout((int) TimeUnit.SECONDS.toMillis(1)).setCompressionEnforced(true)
+				.setFollowRedirect(followRedirects).build();
+		asyncHttpClient = new AsyncHttpClient(config);
+	}
+
+	private void delete(String url) {
+		new RestTemplate().delete(url);
+
+		// try {
+		// asyncHttpClient.prepareDelete(url).execute().get(1,
+		// TimeUnit.SECONDS);
+		// } catch (InterruptedException | ExecutionException | TimeoutException
+		// e) {
+		// // TODO Auto-generated catch block
+		// // e.printStackTrace();
+		// }
+		return;
+	}
+
+	private void put(String url) {
+		new RestTemplate().put(url, null);
+
+		// try {
+		// asyncHttpClient.preparePut(url).execute().get(1, TimeUnit.SECONDS);
+		// } catch (InterruptedException | ExecutionException | TimeoutException
+		// e) {
+		// // TODO Auto-generated catch block
+		// // e.printStackTrace();
+		// }
+		return;
+	}
+
+	private void post(String url) {
+		// new AsyncRestTemplate().postForLocation(url, null);
+
+		asyncHttpClient.preparePost(url).execute();
+		return;
+	}
+
 	private void doLoyalty(long time) {
-		new AsyncRestTemplate().postForLocation(String.format(LOYALTY_URL, remoteConfig.getLoyaltyServiceHost(),
-				remoteConfig.getLoyaltyServicePort(), time), null);
+		post(String.format(LOYALTY_URL, rc.getLoyaltyServiceHost(), rc.getLoyaltyServicePort(), time));
 		return;
 	}
 
 	private void doPost(long time) {
-		new AsyncRestTemplate().postForLocation(
-				String.format(POST_URL, remoteConfig.getPostServiceHost(), remoteConfig.getPostServicePort(), time),
-				null);
+		post(String.format(POST_URL, rc.getPostServiceHost(), rc.getPostServicePort(), time));
 		return;
 	}
 
 	private void doEmail(long time) {
-		new AsyncRestTemplate().postForLocation(
-				String.format(EMAIL_URL, remoteConfig.getEmailServiceHost(), remoteConfig.getEmailServicePort(), time),
-				null);
+		post(String.format(EMAIL_URL, rc.getEmailServiceHost(), rc.getEmailServicePort(), time));
 		return;
 	}
 
 	public void createForAMinute(int repetitions, int interval_seg, int threads, int sleep) {
+		configAsyncHttpClient(threads, interval_seg);
+		_createForAMinute(repetitions, interval_seg, threads, sleep);
+		httpExecutorService.shutdownNow();
+	}
+
+	public void _createForAMinute(int repetitions, int interval_seg, int threads, int sleep) {
 		// BEGIN CONFIG
-		final long interval_nano = TimeUnit.SECONDS.toNanos(interval_seg);
+		final long interval_millis = TimeUnit.SECONDS.toMillis(interval_seg);
 		// END CONFIG
 
 		ExecutorService executor;
@@ -116,12 +176,13 @@ public class CustomerService {
 			long start = System.nanoTime();
 			for (int j = 0; j < threads; j++) {
 				executor.execute(() -> {
-					while (new Date().getTime() - start < interval_nano) {
+					interval_mk: while (new Date().getTime() - start < interval_millis) {
 						createCustomer();
 
 						try {
 							Thread.sleep(sleep);
 						} catch (InterruptedException e) {
+							break interval_mk;
 						}
 					}
 				});
@@ -142,33 +203,33 @@ public class CustomerService {
 	}
 
 	private void clearStatistics() {
-		_clearStatistics(LOYALTY_URL, remoteConfig.getLoyaltyServiceHost(), remoteConfig.getLoyaltyServicePort());
-		_clearStatistics(POST_URL, remoteConfig.getPostServiceHost(), remoteConfig.getPostServicePort());
-		_clearStatistics(EMAIL_URL, remoteConfig.getEmailServiceHost(), remoteConfig.getEmailServicePort());
+		_clearStatistics(LOYALTY_URL, rc.getLoyaltyServiceHost(), rc.getLoyaltyServicePort());
+		_clearStatistics(POST_URL, rc.getPostServiceHost(), rc.getPostServicePort());
+		_clearStatistics(EMAIL_URL, rc.getEmailServiceHost(), rc.getEmailServicePort());
 	}
 
 	private void _clearStatistics(String baseUrl, String host, String port) {
-		new RestTemplate().delete(String.format(baseUrl, host, port, null));
+		delete(String.format(baseUrl, host, port, null));
 	}
 
 	private void printStatistics(int threads, int sleep) {
-		_printStatistics(PRINT_LOYALTY_STATISTICS_URL, remoteConfig.getLoyaltyServiceHost(),
-				remoteConfig.getLoyaltyServicePort(), threads, sleep);
-		_printStatistics(PRINT_POST_STATISTICS_URL, remoteConfig.getPostServiceHost(),
-				remoteConfig.getPostServicePort(), threads, sleep);
-		_printStatistics(PRINT_EMAIL_STATISTICS_URL, remoteConfig.getEmailServiceHost(),
-				remoteConfig.getEmailServicePort(), threads, sleep);
+		_printStatistics(PRINT_LOYALTY_STATISTICS_URL, rc.getLoyaltyServiceHost(), rc.getLoyaltyServicePort(), threads,
+				sleep);
+		_printStatistics(PRINT_POST_STATISTICS_URL, rc.getPostServiceHost(), rc.getPostServicePort(), threads, sleep);
+		_printStatistics(PRINT_EMAIL_STATISTICS_URL, rc.getEmailServiceHost(), rc.getEmailServicePort(), threads,
+				sleep);
 	}
 
 	private void _printStatistics(String url, String host, String port, int threads, int sleep) {
-		new RestTemplate().put(String.format(url, host, port, threads, sleep, null), null);
+		put(String.format(url, host, port, threads, sleep, null));
 	}
 
 	public void iterateCreateForAMinute(int repetitions, int interval, int threads, int start, int increment, int end) {
+		configAsyncHttpClient(threads, interval);
 		for (int i = start; i <= end; i = i + increment) {
-			createForAMinute(repetitions, interval, threads, i);
+			_createForAMinute(repetitions, interval, threads, i);
 		}
-
+		httpExecutorService.shutdownNow();
 	}
 
 }
