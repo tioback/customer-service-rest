535bf67f0b2adff10c0e85626cdebf379282262a
renato <renato@knewin.com>
2016-11-20 15:51:08 -0200

    Incluídos dados de testes.

--- src/main/java/br/ufsc/grad/renatoback/tcc/customer/service/rest/CustomerController.java
+++ src/main/java/br/ufsc/grad/renatoback/tcc/customer/service/rest/CustomerController.java
@@ -1,8 +1,5 @@
 package br.ufsc.grad.renatoback.tcc.customer.service.rest;
 
-import java.util.Date;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CustomerController {
 
-	@Autowired
-	CustomerService customerService;
+	private CustomerService service;
+
+	public CustomerController(CustomerService service) {
+		this.service = service;
+	}
 
 	@RequestMapping(method = RequestMethod.POST)
 	@ResponseStatus(code = HttpStatus.CREATED)
-	public void create() {
-		customerService.createCustomer();
+	public void createCustomer() {
+		service.createCustomer();
 	}
 
 	/**
@@ -33,11 +33,11 @@ public class CustomerController {
 	 */
 	@RequestMapping(path = "/{repetitions}/{interval}/{threads}/{sleep}", method = RequestMethod.GET)
 	@ResponseStatus(code = HttpStatus.OK)
-	public void createForAMinute(@PathVariable(required = true, name = "repetitions") int repetitions,
+	public void createCustomer(@PathVariable(required = true, name = "repetitions") int repetitions,
 			@PathVariable(required = true, name = "interval") int interval,
 			@PathVariable(required = true, name = "threads") int threads,
 			@PathVariable(required = true, name = "sleep") int sleep) {
-		customerService.createForAMinute(repetitions, interval, threads, sleep);
+		service.createForAMinute(repetitions, interval, threads, sleep);
 	}
 
 	/**
@@ -51,18 +51,12 @@ public class CustomerController {
 	 */
 	@RequestMapping(path = "/{repetitions}/{interval}/{threads}/{start}/{increment}/{end}", method = RequestMethod.GET)
 	@ResponseStatus(code = HttpStatus.OK)
-	public void iterateCreateForAMinute(@PathVariable(required = true, name = "repetitions") int repetitions,
+	public void createCustomer(@PathVariable(required = true, name = "repetitions") int repetitions,
 			@PathVariable(required = true, name = "interval") int interval,
 			@PathVariable(required = true, name = "threads") int threads,
 			@PathVariable(required = true, name = "start") int start,
 			@PathVariable(required = true, name = "increment") int increment,
 			@PathVariable(required = true, name = "end") int end) {
-		customerService.iterateCreateForAMinute(repetitions, interval, threads, start, increment, end);
-	}
-
-	@RequestMapping(method = RequestMethod.OPTIONS)
-	@ResponseStatus(code = HttpStatus.OK)
-	public void printTime() {
-		System.out.println("Nano time: " + new Date().getTime());
+		service.iterateCreateForAMinute(repetitions, interval, threads, start, increment, end);
 	}
 }
